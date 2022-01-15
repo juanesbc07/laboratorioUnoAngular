2c1687286b2f56a752ac3d09d6b3ab4390986f28
Juan Esteban Barrero <juanestebanbarrero@hotmail.com>
2022-01-15 16:42:46 -0500

    primeros archivos del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { VideojuegoComponent } from './components/videojuego/videojuego.component';
+import { VideojuegosComponent } from './components/videojuegos/videojuegos.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    BuscadorComponent,
+    VideojuegoComponent,
+    VideojuegosComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/videojuego/videojuego.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-videojuego',
+  templateUrl: './videojuego.component.html',
+  styleUrls: ['./videojuego.component.css']
+})
+export class VideojuegoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
